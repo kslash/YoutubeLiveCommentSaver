adbf28b6b770104f2c8596241f5d8051b436ce9d
kslash <kslasher@gmail.com>
2020-08-27 22:23:47 +0900

    fixup! 処理をシンプル化

--- htmlGetter.py
+++ htmlGetter.py
@@ -1,23 +1,15 @@
 import urllib.request
 import urllib.error
-import chardet
 import codecs
-import sys
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
-}
 
 # urlからhtmlのソースを取得する
 def get_html(url):
-    req = urllib.request.Request(url, None, headers=headers)
-
-    try:
-        request = urllib.request.urlopen(req)
-    except:
-        get_html(url)
-
-    html = request.read()
-    return codecs.decode(html,encoding='utf-8', errors='strict')
+    req = urllib.request.Request(url, None, headers={
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
+    })
+    res = urllib.request.urlopen(req)
+    html = res.read()
+    return codecs.decode(html, encoding='utf-8', errors='strict')
