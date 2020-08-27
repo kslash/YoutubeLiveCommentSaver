6d1ce5d6fc1f62b05fed9dcabc14acae7531ec89
kslash <kslasher@gmail.com>
2020-08-27 22:16:44 +0900

    SSL証明書のエラーを回避

--- htmlGetter.py
+++ htmlGetter.py
@@ -2,6 +2,9 @@ import urllib.request
 import urllib.error
 import chardet
 import codecs
+import sys
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
